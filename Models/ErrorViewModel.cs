49e61f7ad4006d2d18e77ffc9cc2758c257ef19b
Tasauwar Ansari <tasauwar.ansari@leadventure.com>
2023-06-17 12:33:21 +0530

    add project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcgitexample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
